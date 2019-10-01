2ecdff392c6ea09feef5193db010518f21c5f98f
Jaro Habiger <jarohabiger@googlemail.com>
2019-10-01 20:19:17 +0200

    change blink example to use default led

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -10,9 +10,6 @@ use crate::hal::{prelude::*, stm32, delay::Delay};
 use cortex_m_rt::entry;
 use cortex_m::peripheral::Peripherals;
 
-mod front_leds;
-use front_leds::FrontLeds;
-
 #[entry]
 fn main() -> ! {
     let mut peripherals = stm32::Peripherals::take().unwrap();
@@ -22,13 +19,15 @@ fn main() -> ! {
     let mut rcc = peripherals.RCC.configure().sysclk(8.mhz()).freeze(&mut peripherals.FLASH);
     let mut delay = Delay::new(core_peripherals.SYST, &rcc);
 
-    let pa = peripherals.GPIOA.split(&mut rcc);
-    let pb = peripherals.GPIOB.split(&mut rcc);
+    let gpioa = peripherals.GPIOA.split(&mut rcc);
+    let gpiob = peripherals.GPIOB.split(&mut rcc);
 
-    let mut front_leds = FrontLeds::setup(pa);
+    let mut led = cortex_m::interrupt::free(|critical_section| {
+        gpiob.pb1.into_push_pull_output(critical_section)
+    });
 
     loop {
-        delay.delay_ms(33u8);
-        front_leds.next();
+        delay.delay_ms(1000u16);
+        led.toggle();
     }
 }
